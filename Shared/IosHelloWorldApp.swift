61587f2981f1a31c4fdda018e58a562eba518761
jay <weeihua1003@gmail.com>
2022-08-22 16:48:49 +0800

    init

---
+++ Shared/IosHelloWorldApp.swift
@@ -0,0 +1,17 @@
+//
+//  IosHelloWorldApp.swift
+//  Shared
+//
+//  Created by Jay Wu on 2022/8/22.
+//
+
+import SwiftUI
+
+@main
+struct IosHelloWorldApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
